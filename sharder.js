a037396b4e5712b88fab8288ab676d362a2b4e95
Reed <reed@plusreed.com>
2018-07-31 20:12:52 -0400

    Allow specifying number of shards

--- sharder.js
+++ sharder.js
@@ -1,3 +1,6 @@
 const Discord = require('discord.js');
 const Manager = new Discord.ShardingManager('./bear.js');
-Manager.spawn(2); // 5,000 guilds
+const numOfShards = parseInt(process.argv[2]);
+
+console.log(`Starting Bearbot with ${numOfShards | '2'} shards!`);
+Manager.spawn(numOfShards || 2); // 5,000 guilds by default
