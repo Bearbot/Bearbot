9dba6c4e20c1506348ab2d417354626b53462537
Reed <reed@plusreed.com>
2018-10-23 09:09:37 -0400

    add(sharder): add signale scope for shard manager

--- sharder.js
+++ sharder.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
+const signale = require('signale');
+const shardingScope = signale.scope('sharding');
+
 const Manager = new Discord.ShardingManager('./bear.js');
 const numOfShards = parseInt(process.argv[2]);
 
-console.log(`Starting Bearbot with ${numOfShards | '2'} shards!`);
+shardingScope.info(`Starting Bearbot with ${numOfShards | '2'} shards!`);
 Manager.spawn(numOfShards || 2); // 5,000 guilds by default
